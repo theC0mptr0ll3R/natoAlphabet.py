e3bb9693af5f6ee0f08729f45daa3cd6831621cf
theC0mptr0ll3R <moki@m2pi.net>
2024-02-26 00:06:59 +0900

    new file:   natoAlphabet.py

---
+++ natoAlphabet.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+d = {'A':'ALFA', 'B':'BRAVO', 'C':'CHARLIE', 'D':'DELTA', 'E':'ECHO', 'F':'FOXTROT', 'G':'GOLF', 'H':'HOTEL', 'I':'INDIA', 'J':'JULIETT', 'K':'KILO', 'L':'LIMA', 'M':'MIKE', 'N':'NOVEMBER', 'O':'OSCAR', 'P':'PAPA', 'Q':'QUEBEC', 'R':'ROMEO', 'S':'SIERRA', 'T':'TANGO', 'U':'UNIFORM', 'V':'VICTOR', 'W':'WHISKEY', 'X':'XRAY', 'Y':'YANKEE', 'Z':'ZULU', '1':'Wun', '2':'Two', '3':'Tree', '4':'Fower', '5':'Fife', '6':'Six', '7':'Seven', '8':'Ait', '9':'Niner', '0':'Zeero'}
+
+try:
+    arg = sys.argv[1]
+except IndexError:
+    print('ERROR: a word must be entered. Exiting...')
+    sys.exit(0)
+
+for i in arg.upper():
+    print(d[i])
+
+print('\"... read back, please.\"')
